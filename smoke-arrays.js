f6ecaab5f689c2648d81c21133dbd1ee81e3dfcf
Anchihsin <35755824+Anchihsin@users.noreply.github.com>
2018-02-26 02:28:46 -0800

    Update smoke-arrays.js

--- smoke-arrays.js
+++ smoke-arrays.js
@@ -1,31 +1,44 @@
 var x = [];
 var y = [];
-var d = [];
-var color = [];
-
+var r = [];
 
 function setup() {
-  createCanvas(600, 600);
-  colorMode(HSB)
-  for (var i = 0; i < 50; i++){   
-    x[i] = random(0, 600);
-    y[i] = random(0, 600);
-    d[i] = random(0, 600);
-    color[i] = color(200, random(255), 200);
-  }
+  createCanvas(400, 400);
+    for (var i = 0; i < 100; i++) {
+      
+    x[i] = random(200, 210);
+		y[i] = random(0, 300);
+    r[i] = random(1,180);
+	}
 }
-
+  
 function draw() {
   background(0);
   noStroke();
-  
-  // draw ellipses
-  for (var i = 0; i < 50; i++) {
-    fill(color[i]);
-    ellipse(x[i], y[i], d[i]);
 
-		  if (random() < 1) {
-      d[i] = random(0, 200);
-    }
+  // draw smokestack
+  fill(255);
+  rect(195, height, 30, -100);
+
+  // darker as it gets closer to 0
+  for (var i = 0; i < 100; i++) {
+  push();
+  fill(y[i]);
+  translate(x[i], y[i]);
+  rotate(r[i]);
+  rect(-10,-10,10,10);
+  pop();
+  
+    
+  // up 
+  y[i] -= 2;
+  
+  // rotate 0.05 radians ~= 2.8 degrees per frame
+  r[i] += 0.1
+  
+  // if reach past the top a bunch
+  if (y[i] < 0) {
+    y[i] = 250;
   }
+}
 }
