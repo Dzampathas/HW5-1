264053aff88655d086bd80d1d4d7f738cdae9929
Anchihsin <35755824+Anchihsin@users.noreply.github.com>
2018-02-26 01:47:19 -0800

    Update ellipses-arrays.js

--- ellipses-arrays.js
+++ ellipses-arrays.js
@@ -1,1 +1,31 @@
+var x = [];
+var y = [];
+var d = [];
+var color = [];
 
+
+function setup() {
+  createCanvas(600, 600);
+  colorMode(HSB)
+  for (var i = 0; i < 50; i++){   
+    x[i] = random(0, 600);
+    y[i] = random(0, 600);
+    d[i] = random(0, 600);
+    color[i] = color(200, random(255), 200);
+  }
+}
+
+function draw() {
+  background(0);
+  noStroke();
+  
+  // draw ellipses
+  for (var i = 0; i < 50; i++) {
+    fill(color[i]);
+    ellipse(x[i], y[i], d[i]);
+
+		  if (random() < 1) {
+      d[i] = random(0, 200);
+    }
+  }
+}
